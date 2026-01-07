16eb6ecbc4aac3c355ef34909dd98dcd95918625
Alex Rudenko <alexrudenko@chromium.org>
2026-01-07 13:39:04 +0100

    repro 42323022

--- selenium-python/test.py
+++ selenium-python/test.py
@@ -17,6 +17,10 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from PIL import Image
 
 # The chrome and chromedriver installation can take some time.
 # Give 5 minutes to install everything.
@@ -31,13 +35,19 @@ def driver():
     browser_version = "stable"
 
     options = Options()
+    options.add_argument('--window-size=1920,1080')
+    options.add_argument('--force-device-scale-factor=2.0')
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36')
     options.browser_version = browser_version
 
     service = Service(service_args=["--log-path=chromedriver.log", "--verbose"])
 
     driver = webdriver.Chrome(options=options, service=service)
+    driver.set_window_size(1920, 1080)
 
     yield driver
 
@@ -54,5 +64,34 @@ def test_should_be_able_to_navigate_to_google_com(driver):
 
 @pytest.mark.timeout(TIMEOUT)
 def test_issue_reproduction(driver):
-    """Add test reproducing the issue here."""
-    pass
+    """
+    This test reproduces the bug where element.screenshot() does not respect
+    the --force-device-scale-factor option.
+    1. The browser is launched with a device scale factor of 2.0.
+    2. It navigates to https://glints.com/sg.
+    3. It takes a screenshot of the whole page and an element.
+    4. It asserts that the element screenshot has the expected dimensions,
+       which should be scaled by the device scale factor.
+    5. The assertion is expected to fail if the bug is present.
+    """
+    driver.get('https://glints.com/sg')
+
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, 'CareersGridComponentssc__Container-sc-zqphsk-0'))
+    )
+
+    # This is saved with a resolution of 3840x2160
+    driver.save_screenshot('/tmp/screenshot.png')
+
+    # This however, is much smaller - it's the element's size as if the scale ratio is just 1.0.
+    element.screenshot('/tmp/element_screenshot.png')
+
+    expected_width = element.size['width'] * 2
+    expected_height = element.size['height'] * 2
+
+    from PIL import Image
+    with Image.open('/tmp/element_screenshot.png') as img:
+        actual_width, actual_height = img.size
+
+    assert actual_width == expected_width
+    assert actual_height == expected_height
